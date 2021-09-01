7832d755bd51d844f35678c401961d1a14bd268d
Mehran Khaksar <itsaboutmehran@yahoo.com>
2021-09-01 16:32:09 +0430

    set data

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //Api
 import { getCoin } from '../services/api';
 
 const Landing = () => {
 
+    const[coin, setCoin] = useState({});
+
     useEffect(() => {
         const fetchAPI = async () => {
             const data = await getCoin();
+            setCoin(data);
         }
         fetchAPI();
     }, []);
